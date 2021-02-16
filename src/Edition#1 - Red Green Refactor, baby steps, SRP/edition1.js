a32f7a57f57bf9aeae604ba6a75942e7d4789c7b
leiluspocus <laila.atrmouh@gmail.com>
2021-02-16 20:02:05 +0100

    :hankey:

--- src/Edition#1 - Red Green Refactor, baby steps, SRP/edition1.js
+++ src/Edition#1 - Red Green Refactor, baby steps, SRP/edition1.js
@@ -1,4 +1,38 @@
-function edition1(value) {
+function multipleThree(value) {
+    return value % 3 === 0;
+}
+
+function multipleFive(value) {
+    return value % 5 === 0;
+}
+
+
+function multipleFifteen(value) {
+    return value % 15 === 0;
+}
+
+
+
+function fizzbuzz(value) {
+    if (multipleFifteen(value)) {
+        return 'FizzBuzz';
+    }
+    if (multipleThree(value)) {
+        return 'Fizz';
+    }
+    if (multipleFive(value)) {
+        return 'Buzz';
+    }
     return value;
 }
-module.exports = edition1;
+
+function fizzbuzz_output(iterations) {
+    const tableau = [];
+    for ( var i = 1; i <= iterations ; i++) {
+        tableau.push(fizzbuzz(i));
+    }
+    return tableau;
+}
+module.exports = {fizzbuzz, fizzbuzz_output};
+
+
