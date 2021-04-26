02a4c5d70835a5f36e1b45195a2232208880cd44
Marie Terrier <marie.terrier1@gmail.com>
2021-04-26 13:56:02 +0200

    Simple cases v2

--- src/Edition#3 - Red Green Refactor, AAA, DRY/edition3.js
+++ src/Edition#3 - Red Green Refactor, AAA, DRY/edition3.js
@@ -1,4 +1,17 @@
 function toRomanNumerals(value) {
-    return 'I';
+    const keyValues = {
+        1:'I',
+        0: '',
+        5:'V',
+        10:'X'
+    }
+
+    if(keyValues[value] !== undefined) {
+      return keyValues[value];
+    }
+
+    return 'II'
+
+
 }
 module.exports = toRomanNumerals;
--- src/Edition#3 - Red Green Refactor, AAA, DRY/edition3.test.js
+++ src/Edition#3 - Red Green Refactor, AAA, DRY/edition3.test.js
@@ -11,5 +11,43 @@ describe('Edition#3', () => {
             // Assert
             expect(actual).toBe(expected);
         });
+        test('Simple case - Given 0, should return string vide ""', () => {
+            // Arrange
+            let input = 0;
+            let expected = '';
+            // Act
+            let actual = toRomanNumerals(input);
+            // Assert
+            expect(actual).toBe(expected);
+        });
+        test('Simple case - Given 5, should return V', () => {
+            // Arrange
+            let input = 5;
+            let expected = 'V';
+            // Act
+            let actual = toRomanNumerals(input);
+            // Assert
+            expect(actual).toBe(expected);
+        });
+
+        test('Simple case - Given 10, should return X', () => {
+            // Arrange
+            let input = 10;
+            let expected = 'X';
+            // Act
+            let actual = toRomanNumerals(input);
+            // Assert
+            expect(actual).toBe(expected);
+        });
+
+        test('Middle case - Given 2, should return II', () => {
+            // Arrange
+            let input = 2;
+            let expected = 'II';
+            // Act
+            let actual = toRomanNumerals(input);
+            // Assert
+            expect(actual).toBe(expected);
+        });
     })
 })
