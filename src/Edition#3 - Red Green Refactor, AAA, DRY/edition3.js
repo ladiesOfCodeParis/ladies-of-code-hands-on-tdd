a39d4e791f2fd314513f4112af8730fe92353605
Romy Alula <goumiesone.dev@gmail.com>
2021-06-30 19:13:07 +0200

    Add simple cases

--- src/Edition#3 - Red Green Refactor, AAA, DRY/edition3.js
+++ src/Edition#3 - Red Green Refactor, AAA, DRY/edition3.js
@@ -1,4 +1,7 @@
 function toRomanNumerals(value) {
-    return value;
+    if(value === 1) return 'I';
+    if(value === 2) return 'II';
+    if(value === 3) return 'III';
+    return '';
 }
 module.exports = toRomanNumerals;
--- src/Edition#3 - Red Green Refactor, AAA, DRY/edition3.test.js
+++ src/Edition#3 - Red Green Refactor, AAA, DRY/edition3.test.js
@@ -2,10 +2,40 @@ const toRomanNumerals = require('./edition3');
 
 describe('Edition#3', () => {
     describe('Roman Numerals', () => {
-        test('Given a value, should return the given value', () => {
+        test('Given 0, should return empty string', () => {
             // Arrange
             let input = 0;
-            let expected = 1;
+            let expected = '';
+            // Act
+            let actual = toRomanNumerals(input);
+            // Assert
+            expect(actual).toBe(expected);
+        });
+
+        test('Given 1, should return I', () => {
+            // Arrange
+            let input = 1;
+            let expected = 'I';
+            // Act
+            let actual = toRomanNumerals(input);
+            // Assert
+            expect(actual).toBe(expected);
+        });
+
+        test('Given 2, should return II', () => {
+            // Arrange
+            let input = 2;
+            let expected = 'II';
+            // Act
+            let actual = toRomanNumerals(input);
+            // Assert
+            expect(actual).toBe(expected);
+        });
+
+        test('Given , should return ', () => {
+            // Arrange
+            let input = 3;
+            let expected = 'III';
             // Act
             let actual = toRomanNumerals(input);
             // Assert
