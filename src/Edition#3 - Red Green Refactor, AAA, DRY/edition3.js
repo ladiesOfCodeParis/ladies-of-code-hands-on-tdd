b082c1b1563d31dad51756f70c188e90919641cb
Romy Alula <goumiesonedev@gmail.com>
2021-08-22 14:03:11 +0200

    Revert "Add simple cases"
    This reverts commit 5afe96734ead50db5fd5cef35e0fa98843810023.

--- src/Edition#3 - Red Green Refactor, AAA, DRY/edition3.js
+++ src/Edition#3 - Red Green Refactor, AAA, DRY/edition3.js
@@ -1,7 +1,4 @@
 function toRomanNumerals(value) {
-    if(value === 1) return 'I';
-    if(value === 2) return 'II';
-    if(value === 3) return 'III';
-    return '';
+    return value;
 }
 module.exports = toRomanNumerals;
