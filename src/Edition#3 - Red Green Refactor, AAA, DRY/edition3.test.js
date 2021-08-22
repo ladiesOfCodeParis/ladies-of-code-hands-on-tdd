5fbc6426e36858ed49301edd1e01ec7bded20929
Romy Alula <goumiesonedev@gmail.com>
2021-08-22 14:03:11 +0200

    Revert "Add simple cases"
    This reverts commit 5afe96734ead50db5fd5cef35e0fa98843810023.

--- src/Edition#3 - Red Green Refactor, AAA, DRY/edition3.test.js
+++ src/Edition#3 - Red Green Refactor, AAA, DRY/edition3.test.js
@@ -2,40 +2,10 @@ const toRomanNumerals = require('./edition3');
 
 describe('Edition#3', () => {
     describe('Roman Numerals', () => {
-        test('Given 0, should return empty string', () => {
+        test('Given a value, should return the given value', () => {
             // Arrange
             let input = 0;
-            let expected = '';
-            // Act
-            let actual = toRomanNumerals(input);
-            // Assert
-            expect(actual).toBe(expected);
-        });
-
-        test('Given 1, should return I', () => {
-            // Arrange
-            let input = 1;
-            let expected = 'I';
-            // Act
-            let actual = toRomanNumerals(input);
-            // Assert
-            expect(actual).toBe(expected);
-        });
-
-        test('Given 2, should return II', () => {
-            // Arrange
-            let input = 2;
-            let expected = 'II';
-            // Act
-            let actual = toRomanNumerals(input);
-            // Assert
-            expect(actual).toBe(expected);
-        });
-
-        test('Given , should return ', () => {
-            // Arrange
-            let input = 3;
-            let expected = 'III';
+            let expected = 1;
             // Act
             let actual = toRomanNumerals(input);
             // Assert
